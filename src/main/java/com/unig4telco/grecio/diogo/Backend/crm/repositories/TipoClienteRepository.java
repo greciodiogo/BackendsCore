2fe7626097e056ba7c1c944a918db82c84bac7ad
Grécio Diogo <fonebahia8@gmail.com>
2024-08-24 12:01:47 +0100

    feat: Add direccao endpoint and countries list endpoint

--- src/main/java/com/unig4telco/grecio/diogo/Backend/crm/repositories/TipoClienteRepository.java
+++ src/main/java/com/unig4telco/grecio/diogo/Backend/crm/repositories/TipoClienteRepository.java
@@ -1,6 +1,7 @@
 package com.unig4telco.grecio.diogo.Backend.crm.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,7 @@ import com.unig4telco.grecio.diogo.Backend.crm.domain.TipoCliente;
 
 public interface TipoClienteRepository extends JpaRepository<TipoCliente, Long>{
     List<TipoCliente> findAll();
+
+     Optional<TipoCliente> findBySlug(String slug);
+    
 }
