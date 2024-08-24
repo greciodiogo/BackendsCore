23d81dbdd938929cb503f718419ebc17a043a40d
Grécio Diogo <fonebahia8@gmail.com>
2024-08-24 12:02:33 +0100

    Revert "feat: Add direccao endpoint and countries list endpoint"
    This reverts commit 2fe7626097e056ba7c1c944a918db82c84bac7ad.

--- src/main/java/com/unig4telco/grecio/diogo/Backend/crm/repositories/DireccaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.unig4telco.grecio.diogo.Backend.crm.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-import com.unig4telco.grecio.diogo.Backend.crm.domain.Direccao;
-
-public interface DireccaoRepository extends JpaRepository<Direccao, Long>{
-    List<Direccao> findAll();
-} 
--- src/main/java/com/unig4telco/grecio/diogo/Backend/crm/repositories/TipoClienteRepository.java
+++ src/main/java/com/unig4telco/grecio/diogo/Backend/crm/repositories/TipoClienteRepository.java
@@ -1,7 +1,6 @@
 package com.unig4telco.grecio.diogo.Backend.crm.repositories;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,7 +8,4 @@ import com.unig4telco.grecio.diogo.Backend.crm.domain.TipoCliente;
 
 public interface TipoClienteRepository extends JpaRepository<TipoCliente, Long>{
     List<TipoCliente> findAll();
-
-     Optional<TipoCliente> findBySlug(String slug);
-    
 }
