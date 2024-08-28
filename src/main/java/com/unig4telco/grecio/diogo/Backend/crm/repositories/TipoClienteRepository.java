d94d2f12cc08f5a9d6ce5c96ec443dfa7e974ef8
Grécio Diogo <fonebahia8@gmail.com>
2024-08-28 22:24:40 +0100

    fix: refactor on repositories classes

--- src/main/java/com/unig4telco/grecio/diogo/Backend/crm/repositories/TipoClienteRepository.java
+++ src/main/java/com/unig4telco/grecio/diogo/Backend/crm/repositories/TipoClienteRepository.java
@@ -3,12 +3,10 @@ package com.unig4telco.grecio.diogo.Backend.crm.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.unig4telco.grecio.diogo.Backend.crm.domain.TipoCliente;
 
-public interface TipoClienteRepository extends JpaRepository<TipoCliente, Long>{
-    List<TipoCliente> findAll();
-
+public interface TipoClienteRepository extends JpaRepository<TipoCliente, Long>, JpaSpecificationExecutor<TipoCliente>{
     List<TipoCliente> findBySlug(String slug);
-    
 }
--- src/main/java/com/unig4telco/grecio/diogo/Backend/utilitarios/repositories/DocumentoRepository.java
+++ src/main/java/com/unig4telco/grecio/diogo/Backend/utilitarios/repositories/DocumentoRepository.java
@@ -1,11 +1,10 @@
 package com.unig4telco.grecio.diogo.Backend.utilitarios.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.unig4telco.grecio.diogo.Backend.utilitarios.domain.Documento;
 
-public interface DocumentoRepository extends JpaRepository <Documento, Long>{
-    List<Documento> findAll();
+public interface DocumentoRepository extends JpaRepository <Documento, Long>, JpaSpecificationExecutor<Documento>{
+
 }
