2e1ffcbd52b7b41d9c905fee0bb48ad23ca70422
Grécio Diogo <fonebahia8@gmail.com>
2024-08-30 12:19:27 +0100

    refactor: refactor and add configs module added

--- src/main/java/com/unig4telco/grecio/diogo/Backend/configs/direccao/_repositories/DireccaoAreaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.unig4telco.grecio.diogo.Backend.configs.direccao._repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import com.unig4telco.grecio.diogo.Backend.configs.direccao._domain.DireccaoArea;
-
-public interface DireccaoAreaRepository extends JpaRepository<DireccaoArea, Long>, JpaSpecificationExecutor<DireccaoArea>{}
